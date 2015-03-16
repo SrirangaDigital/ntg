196cc305b6d12c8991f6c799c9fed626d1af37c9
suresh07 <soori_rozee@yahoo.com>
2015-03-16 17:16:24 +0530

    Commit No. 4 Query changed (drop db if exists)

--- generate.sh
+++ generate.sh
@@ -5,7 +5,7 @@ db="ntg"
 usr="root"
 pwd="mysql"
 
-echo "drop database ntg; create database ntg;" | /usr/bin/mysql -uroot -pmysql
+echo "drop database if exists ntg; create database ntg;" | /usr/bin/mysql -uroot -pmysql
 perl insert_author.pl $host $db $usr $pwd
 perl insert_feat.pl $host $db $usr $pwd
 perl insert_articles.pl $host $db $usr $pwd
