70076e6f03d8f78b695020f827b470b6e63b5481
arjunkashyap <arjun.kashyap@yahoo.co.in>
2015-03-25 19:00:59 +0530

    Commit No. 41 - collation and charset issues fixed

--- generate.sh
+++ generate.sh
@@ -5,7 +5,7 @@ db="ntg"
 usr="root"
 pwd='mysql'
 
-echo "CREATE DATABASE IF NOT EXISTS ntg DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;" | /usr/bin/mysql -uroot -p$pwd
+echo "CREATE DATABASE IF NOT EXISTS ntg CHARACTER SET utf8 COLLATE utf8_general_ci;" | /usr/bin/mysql -uroot -p$pwd
 echo "CREATE TABLE IF NOT EXISTS details(name varchar(1000),email varchar(100),info varchar(5000),password varchar(100),interest varchar(2000),misc varchar(1000),isverified varchar(1),visitcount int(5), userid int(6) auto_increment, primary key(userid)) ENGINE=MyISAM;" | /usr/bin/mysql -uroot -p$pwd ntg
 echo "CREATE TABLE IF NOT EXISTS reset (hash varchar(100), email varchar(100), name varchar(1000), password varchar(100), timestamp varchar(100), resetid int(6) AUTO_INCREMENT, PRIMARY KEY (resetid)) ENGINE=MyISAM;" | /usr/bin/mysql -uroot -p$pwd ntg
 
